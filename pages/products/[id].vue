41c425f6b1485b461db64e2fb40d86d6ed44786c
vidhi-7span <vidhi@7span.com>
2024-02-25 18:06:06 +0530

    Error Page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,11 @@ const url = "https://fakestoreapi.com/products/" + id;
 
 // Fetch the Products
 const { data: products } = await useFetch(url, { key: id });
+
+if (!products.value) {
+  throw createError({ statusCode: 404, statusMessage: "Product not found" });
+}
+
 definePageMeta({
   layout: "products",
 });
